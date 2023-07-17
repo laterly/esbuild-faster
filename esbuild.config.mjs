5b7e363e1985f221286fc076136c68b8a74735f4
laterly <2608086516@qq.com>
2023-07-18 01:08:35 +0800

    feat: server

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -51,20 +51,20 @@ http
     };
 
     // Forward each incoming request to esbuild
-    // const proxyReq = http.request(options, (proxyRes) => {
-    //   // If esbuild returns "not found", send a custom 404 page
-    //   if (proxyRes.statusCode === 404) {
-    //     res.writeHead(404, { "Content-Type": "text/html" });
-    //     res.end("<h1>A custom 404 page</h1>");
-    //     return;
-    //   }
+    const proxyReq = http.request(options, (proxyRes) => {
+      // If esbuild returns "not found", send a custom 404 page
+      if (proxyRes.statusCode === 404) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>A custom 404 page</h1>");
+        return;
+      }
 
-    //   // Otherwise, forward the response from esbuild to the client
-    //   res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    //   // proxyRes.pipe(res, { end: true });
-    // });
+      // Otherwise, forward the response from esbuild to the client
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
+      // proxyRes.pipe(res, { end: true });
+    });
 
     // Forward the body of the request to esbuild
-    // req.pipe(proxyReq, { end: true });
+    req.pipe(proxyReq, { end: true });
   })
   .listen(3000);
