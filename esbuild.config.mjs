bb2fb3cbe43f0c1cb5bd95ec7034b7b0cc86d731
laterly <2608086516@qq.com>
2023-07-18 00:35:06 +0800

    feat: comper

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -6,44 +6,59 @@
 //   outfile: 'dist/index.js',
 // })
 
-import * as esbuild from 'esbuild'
-import http from 'node:http'
+import * as esbuild from "esbuild";
+import http from "node:http";
+import { sassPlugin } from "esbuild-sass-plugin";
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
 
 // Start esbuild's server on a random local port
 let ctx = await esbuild.context({
-  // ... your build options go here ...
-  entryPoints: ['src/index.tsx'],
-  bundle: true,
-  outfile: 'dist/index.js',
-})
-
-// The return value tells us where esbuild's local server is
-let { host, port } = await ctx.serve({ servedir: '.' })
-
-// Then start a proxy server on port 3000
-http.createServer((req, res) => {
-  const options = {
-    hostname: host,
-    port: port,
-    path: req.url,
-    method: req.method,
-    headers: req.headers,
-  }
-
-  // Forward each incoming request to esbuild
-  const proxyReq = http.request(options, proxyRes => {
-    // If esbuild returns "not found", send a custom 404 page
-    if (proxyRes.statusCode === 404) {
-      res.writeHead(404, { 'Content-Type': 'text/html' })
-      res.end('<h1>A custom 404 page</h1>')
-      return
-    }
-
-    // Otherwise, forward the response from esbuild to the client
-    res.writeHead(proxyRes.statusCode, proxyRes.headers)
-    proxyRes.pipe(res, { end: true })
+    // ... your build options go here ...
+    logLevel: 'debug',
+    metafile: true,
+    entryPoints: ["src/index.tsx"],
+    bundle: true,
+    outfile: "dist/index.js",
+    plugins: [
+      sassPlugin({
+        async transform(source) {
+          const { css } = await postcss([autoprefixer]).process(source);
+          return css;
+        },
+      }),
+    ],
   })
 
-  // Forward the body of the request to esbuild
-  req.pipe(proxyReq, { end: true })
-}).listen(3000)
+// The return value tells us where esbuild's local server is
+let { host, port } = await ctx.serve({ servedir: "." });
+
+// Then start a proxy server on port 3000
+http
+  .createServer((req, res) => {
+    const options = {
+      hostname: host,
+      port: port,
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+    };
+
+    // Forward each incoming request to esbuild
+    const proxyReq = http.request(options, (proxyRes) => {
+      // If esbuild returns "not found", send a custom 404 page
+      if (proxyRes.statusCode === 404) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>A custom 404 page</h1>");
+        return;
+      }
+
+      // Otherwise, forward the response from esbuild to the client
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
+      proxyRes.pipe(res, { end: true });
+    });
+
+    // Forward the body of the request to esbuild
+    req.pipe(proxyReq, { end: true });
+  })
+  .listen(3000);
--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import './app.css';
 const App: React.FC = () => {
   const [state, setState] = useState<number>(0);
   return (
     <div>
-      <p>数字:{state}</p>
+      <p className={'box'}>数字3  :{state}</p>
       <button onClick={() => setState((prev) => prev + 1)}>点击呀</button>
     </div>
   );
